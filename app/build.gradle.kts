690e754e7fad93e4c1ee017baa95adec22b38485
Senthilmurugan Krishnan <sen.anju5@outlook.com>
2024-06-23 11:32:52 -0500

    upgraded the gradle versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,13 +47,14 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-    implementation("androidx.databinding:databinding-runtime:8.3.1")
+    implementation("androidx.databinding:databinding-runtime:8.5.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-android:2.8.2")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -64,7 +65,7 @@ dependencies {
     implementation("com.todkars:shimmer-recyclerview:0.4.1")
 
     //Gson
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.code.gson:gson:2.11.0")
 
     //retrofit
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
